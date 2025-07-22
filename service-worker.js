9c4208c806257ca42551d0ef0456bf08da36fa1f
Lukman <lukmannrhkm80@gmail.com>
2025-07-22 19:56:19 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Nama cache diubah ke v3 untuk memaksa pembaruan
-const CACHE_NAME = 'truth-or-dare-cache-v3'; 
+// Nama cache diubah ke v5 untuk memaksa pembaruan
+const CACHE_NAME = 'truth-or-dare-cache-v5'; 
 const urlsToCache = [
     '/',
     './', // Menambahkan path root relatif
